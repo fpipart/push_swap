7c227cf4bef5dfbb09936ae7b88a2055017a2a26
Franck PIPART <fpipart@e1r9p4.42.fr>
2017-01-23 14:05:15 +0100

    Add a check to compare input with int size

--- srcs/ft_atoi_checker.c
+++ srcs/ft_atoi_checker.c
@@ -6,13 +6,33 @@
 /*   By: fpipart <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/04 16:30:44 by fpipart           #+#    #+#             */
-/*   Updated: 2017/01/12 19:06:39 by fpipart          ###   ########.fr       */
+/*   Updated: 2017/01/15 12:52:02 by fpipart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-int		ft_atoi_checker(char *str, char **error)
+static int	check_int(char *str, int sgn)
+{
+	int i;
+
+	i = 0;
+	while (str[i] < '0' || str[i] > '9')
+		str++;
+	while (str[i] >= '0' && str[i] <= '9')
+		i++;
+	if (i == 10 && sgn == 1)
+		if (ft_strcmp(str, "2147483647") > 0)
+			return (0);
+	if (i == 10 && sgn == -1)
+		if (ft_strcmp(str, "2147483648") > 0)
+			return (0);
+	if (i > 10)
+		return (0);
+	return (1);
+}
+
+int			ft_atoi_checker(char *str, char **error)
 {
 	int i;
 	int sgn;
@@ -34,7 +54,7 @@ int		ft_atoi_checker(char *str, char **error)
 		res = res + str[i] - '0';
 		i++;
 	}
-	if (!str[i])
+	if (!str[i] && check_int(str, sgn))
 		*error = NULL;
 	else
 		*error = "error";
